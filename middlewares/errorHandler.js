fa30a4f05b8d25f5739c34cb92c218f42a619a50
João Linhares Gomes <linharesgomes.joao@gmail.com>
2023-05-01 22:37:29 +0100

    log error stack in errorHandler middleware

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,12 +1,14 @@
 function errorHandler (err, req, res, next) {
-    console.error(err.message);
+    console.error(err.stack);
 
 
     const errStatus = err.statusCode || 500;
     const errMsg = err.message || 'Something went wrong!';
 
     res.status(errStatus)
-        .json({ error: errMsg });
+        .json({ 
+            error: errMsg 
+        });
 }
 
 module.exports = errorHandler;
